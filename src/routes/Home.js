6647d3454569071d01ea4a03f4d07a81ab093b48
김강태 <kangtae49@gmail.com>
2020-12-27 20:33:07 +0900

    first gql

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,28 @@
 import React from "react";
 
-const Home = () => <h1>Home</h1>;
+import {gql} from "apollo-boost";
+import {useQuery} from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`
+
+
+const Home = () => {
+    const {loading, error, data} = useQuery(GET_MOVIES);
+    console.log(loading, error, data);
+    if (loading){
+        return "loading...";
+    }
+    if(data && data.movies){
+        return data.movies.map((m, index)=><h1 key={index}>{m.id}</h1>);
+    }
+    return <h1>Home</h1>;
+}
 
 export default Home;
